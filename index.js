47326d9d54585d1798f3e2189c6a023706f51fe3
Maurício <mauriciotomazz@gmail.com>
2022-04-06 01:29:06 -0700

    Implement snake movement

--- index.js
+++ index.js
@@ -1,30 +1,87 @@
-function board() {
-  // Board size is 20 tiles (width) per 16 tiles (height)
-  const WIDTH = 28
-  const HEIGHT = 16
+import { segmentBuilder, snakeBuilder, snakeHistoryBuilder, drawSnake } from './scripts/snake.js'
+
+// Board size is 20 tiles (width) per 16 tiles (height)
+const WIDTH = 28
+const HEIGHT = 16
+
+function drawBoard() {
 
   const boardInnerEl = document.querySelector('#game-board')
   const tileSize = boardInnerEl.scrollHeight / HEIGHT
   
+  const createTileElement = (isEven, id) => {
+    const tileEl = document.createElement('div')
+    tileEl.classList.add('tile', isEven ? 'tile-even' : 'tile-odd')
+    tileEl.style.height = `${tileSize}px`
+    tileEl.style.width = `${tileSize}px`
+    tileEl.id = id
+    return tileEl
+  }
+
   for (let i = 0; i < WIDTH; i++) {
     for (let j = 0; j < HEIGHT; j++) {
       const isEven = (j + i) % 2 === 0
-      const newTileEl = document.createElement('div')
-      newTileEl.classList.add('tile', isEven ? 'tile-even' : 'tile-odd')
-      newTileEl.style.height = `${tileSize}px`
-      newTileEl.style.width = `${tileSize}px`
-      newTileEl.id = `tile-${i}-${j}`
-      boardInnerEl.appendChild(newTileEl)
-      boardInnerEl.style.width = `${tileSize * WIDTH}px`
+      boardInnerEl.appendChild(createTileElement(isEven, `tile-${i}-${j}`))
     }
   }
+  
+  boardInnerEl.style.width = `${tileSize * WIDTH}px`
 }
 
-(function() {
-
+function setRootElementSize() {
   const rootEl = document.querySelector('#game-root')
   rootEl.style.width = `${window.innerWidth}px`
   rootEl.style.height = `${window.innerHeight}px`
+}
 
-  board()
+(function() {
+  const initialSnake = snakeBuilder(
+    segmentBuilder(12, 8),
+    segmentBuilder(13, 8),
+    segmentBuilder(14, 8),
+  )
+  const snakeHistory = snakeHistoryBuilder(initialSnake)
+  let direction = 'RIGHT'
+
+  setRootElementSize()
+  drawBoard()
+  drawSnake(snakeHistory)
+
+  window.addEventListener('keydown', e => {
+    const LEFT_RIGHT = ['LEFT', 'RIGHT'].includes(direction)
+    const UP_DOWN = ['UP', 'DOWN'].includes(direction)
+    switch (e.key) {
+      case 'ArrowUp':
+        if (LEFT_RIGHT) {
+          direction = 'UP';
+        }
+        break
+      case 'ArrowDown':
+        if (LEFT_RIGHT) {
+          direction = 'DOWN';
+        }
+        break
+      case 'ArrowRight':
+        if (UP_DOWN) {
+          direction = 'RIGHT';
+        }
+        break
+      case 'ArrowLeft':
+        if (UP_DOWN) {
+          direction = 'LEFT';
+        }
+        break
+      default:
+        // ignore other keys
+        break
+    }
+  })
+
+  const intervalId = setInterval(() => {
+    if (!snakeHistory.move(direction)) {
+      clearInterval(intervalId)
+      return
+    }
+    drawSnake(snakeHistory)
+  }, 300)
 })();
---
+++ scripts/snake.js
@@ -0,0 +1,110 @@
+// Board size is 20 tiles (width) per 16 tiles (height)
+const WIDTH = 28
+const HEIGHT = 16
+
+function segmentBuilder(x, y) {
+  // Returns an object that represents the position of the snake's segments
+  return { x, y }
+}
+
+function snakeBuilder(...snakeSegments) {
+  const segments = snakeSegments
+
+  const head = () => segments[segments.length - 1]
+
+  const body = () => segments.slice(0, segments.length - 1).reverse()
+
+  // Returns an object that represents the snake body with all its segments
+  return {
+    segments,
+    head,
+    body,
+  }
+}
+
+function snakeHistoryBuilder(initialSnake) {
+  // snakeHistory stores each version of snake in time
+  // every time tick the last snake version is copied and changed
+  // keeping the previous one in the array
+  const history = [initialSnake]
+
+  const getCurrentSnake = () => history[history.length - 1]
+
+  const getPrevSnake = () => history.length > 1 ? history[history.length - 2] : null
+
+  const move = (direction) => {
+    if (detectCollision(direction)) {
+      console.log(history)
+      return false
+    }
+
+    switch (direction) {
+      case 'RIGHT':
+        return moveRight()
+      case 'DOWN':
+        return moveDown()
+      default:
+        throw new Error('Invalid direction')
+    }
+  }
+
+  const moveRight = () => {
+    const currentSnake = getCurrentSnake()
+    const nextSnakeBody = currentSnake.segments.map(segment => segmentBuilder(segment.x + 1, segment.y))
+    history.push(snakeBuilder(...nextSnakeBody))
+    return true
+  }
+
+  const moveDown = () => {
+    const currentSnake = getCurrentSnake()
+    const currentHead = currentSnake.head()
+    const newHead = segmentBuilder(currentHead.x, currentHead.y + 1)
+    let prevSegment = newHead
+
+    const newBody = currentSnake.body().map(segment => {
+      const prevX = prevSegment.x
+      prevSegment = segment
+      if (segment.x !== newHead.x) {
+        return segmentBuilder(prevX, segment.y)
+      }
+      return segmentBuilder(segment.x, segment.y + 1)
+    })
+
+    history.push(snakeBuilder(...newBody, newHead))
+    return true
+  }
+
+  const detectCollision = (direction) => {
+    switch (direction) {
+      case 'RIGHT':
+        return getCurrentSnake().head().x === WIDTH - 1
+      case 'DOWN':
+        return getCurrentSnake().head().y === HEIGHT - 1
+      default:
+        throw new Error('Invalid direction')
+    }
+  }
+
+  return {
+    getCurrentSnake,
+    getPrevSnake,
+    move,
+  }
+}
+
+function drawSnake(snakeHistory) {
+  const changeTileColor = (snake, color) => {
+    snake.segments.forEach(segment => {
+      const tileEl = document.querySelector(`#tile-${segment.x}-${segment.y}`)
+      tileEl.style.background = color
+    })
+  }
+  // clear previous snake
+  if (snakeHistory.getPrevSnake()) {
+    changeTileColor(snakeHistory.getPrevSnake(), null)
+  }
+  // draw current snake
+  changeTileColor(snakeHistory.getCurrentSnake(), '#005500')
+}
+
+export { segmentBuilder, snakeBuilder, snakeHistoryBuilder, drawSnake }
